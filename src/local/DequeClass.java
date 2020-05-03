ea19bbe2ad0eef1efd2d5fe0e85e3fa0e789023f
Ivan <semen_gen@tut.by>
2020-05-03 19:58:05 +0300

    Очередь в поликлинике

--- src/local/DequeClass.java
+++ src/local/DequeClass.java
@@ -1,5 +1,35 @@
 package local;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class DequeClass {
+
+    public final int MAX_VISITOR = 30;
+    public static int currentIndex;
+
+    private Deque<Integer> queue;
+
+    public DequeClass(int currentIndex) {
+        DequeClass.currentIndex = currentIndex;
+        queue = new LinkedList<>();
+
+        for (int i = 1; i <= currentIndex; i++) {
+            queue.add(i);
+        }
+    }
+
+    public Deque<Integer> getQueue() {
+        return queue;
+    }
+
+    public void printPatient(String patient) {
+        System.out.println("Пациент № " + patient);
+    }
+
+    @Override
+    public String toString() {
+        return queue.toString();
+    }
 
 }
--- src/local/Main.java
+++ src/local/Main.java
@@ -3,6 +3,16 @@ package local;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        DequeClass hospital = new DequeClass(5);
+        int counter = 0;
+
+        System.out.println("Очередь в поликлинике");
+        while (hospital.getQueue().size() > 0) {
+            counter++;
+            if (DequeClass.currentIndex < hospital.MAX_VISITOR) hospital.getQueue().add(++DequeClass.currentIndex);
+            if (counter % 3 == 0) hospital.printPatient(hospital.getQueue().pollLast().toString() + " без очереди");
+            else hospital.printPatient(hospital.getQueue().poll().toString());
+        }
     }
+
 }
